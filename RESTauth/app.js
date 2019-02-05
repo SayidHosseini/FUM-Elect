a60c74b041adf2e623637bacda6670b17aa05697
unknown <sayidhosseini@hotmail.com>
2019-02-05 15:30:27 +0330

    removed debugging lines

--- RESTauth/app.js
+++ RESTauth/app.js
@@ -10,8 +10,6 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connect(dbConfig.DB);
 
-console.log("OK!");
-
 //var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,8 +19,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-console.log("VIEW ENGINE");
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -31,8 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-console.log("USERROUTER");
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
