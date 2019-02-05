9163265ecc890c5744dc3f0a94b2490521bc9414
Ubuntu <ubuntu@ip-172-31-47-32.ec2.internal>
2019-02-05 17:14:17 +0000

    no idea whats changing!

--- build.sh
+++ build.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+clear
+
 cd master/
-mvn clean 
+echo "cleaning master"
+mvn clean
+echo "installing master"
 mvn install
 
 cd ../validator/
+echo "cleaning validator"
 mvn clean
+echo "installing validator"
 mvn install
 
+cd ../electionportal/
+echo "cleaning electionportal"
+mvn clean
+echo "installing electionportal"
+mvn install
+
+echo "docker-compose down"
 sudo docker-compose down
+echo "docker-compose up"
 sudo docker-compose up
