eb045d002c10faec26d38f175b1dae7001687dbd
ardalanfp <aforoughipour@gmail.com>
2019-02-05 18:21:07 +0330

    60 second sleep before starting

--- master/src/main/java/main/CloudCourseProject/MasterApplication.java
+++ master/src/main/java/main/CloudCourseProject/MasterApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MasterApplication {
     public static void main(String[] args) {
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(MasterApplication.class, args);
     }
 }
