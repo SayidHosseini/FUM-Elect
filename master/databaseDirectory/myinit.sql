967efc5eb788ee080cc0fd6fed9c90e020a22028
unknown <sayidhosseini@hotmail.com>
2019-02-09 13:30:43 +0330

    Finalizing...

--- master/databaseDirectory/myinit.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE CloudProjectMaster ;
-CREATE USER 'cloudProjectMasterUser'@'%' IDENTIFIED BY '12345';
-GRANT ALL PRIVILEGES ON CloudProjectMaster.* TO 'cloudProjectMasterUser'@'%';
--- master/src/main/java/main/CloudCourseProject/repository/ElectionRepository.java
+++
@@ -1,7 +0,0 @@
-package main.CloudCourseProject.repository;
-
-import main.CloudCourseProject.model.Election;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ElectionRepository extends JpaRepository<Election, Integer> {
-}
